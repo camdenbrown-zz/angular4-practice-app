4a03a95f4f731251d7954ac77b3117c661acb750
Camden Brown <camden.brown@metova.com>
2017-08-07 15:19:36 -0500

    server component implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+//Components
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
